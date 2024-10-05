90f9d2c96fa6f625c8f53a45659b427e86b8edc2
rikeshkumarsingh <rikeshsingh465@gmail.com>
2024-10-05 18:37:23 +0530

    circuite breaker implemented

--- userservice/src/main/java/com/userservice/Model/Usermanagemant.java
+++ userservice/src/main/java/com/userservice/Model/Usermanagemant.java
@@ -6,12 +6,18 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usermanagemant {
 
 	@Id
--- userservice/src/main/java/com/userservice/controller/UsermanagemantController.java
+++ userservice/src/main/java/com/userservice/controller/UsermanagemantController.java
@@ -16,46 +16,58 @@ import org.springframework.web.bind.annotation.RestController;
 import com.userservice.Model.Usermanagemant;
 import com.userservice.service.UsermanagemantService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/users")
 public class UsermanagemantController {
-	
+
 	@Autowired
 	UsermanagemantService usermanagemantService;
-	
+
 	@PostMapping("/save")
-	ResponseEntity<Usermanagemant>saveuser(@RequestBody Usermanagemant usermanagemant){
+	ResponseEntity<Usermanagemant> saveuser(@RequestBody Usermanagemant usermanagemant) {
 		try {
 			Usermanagemant saveuser = this.usermanagemantService.saveuser(usermanagemant);
-			return new ResponseEntity<Usermanagemant>(saveuser,HttpStatus.OK);
+			return new ResponseEntity<Usermanagemant>(saveuser, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<Usermanagemant>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}		
+		}
 	}
-	
-	
+
 	@GetMapping("/getall")
-	ResponseEntity<List<Usermanagemant>>getAll(){
+	ResponseEntity<List<Usermanagemant>> getAll() {
 		try {
 			List<Usermanagemant> all = this.usermanagemantService.getAll();
-			return new ResponseEntity<List<Usermanagemant>>(all,HttpStatus.OK);
+			return new ResponseEntity<List<Usermanagemant>>(all, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<List<Usermanagemant>>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}		
+		}
 	}
-	
+
 	@GetMapping("/get/{id}")
-	ResponseEntity<Usermanagemant>getById(@PathVariable String id){
+//	@CircuitBreaker(name = "ratingHotleBreaker", fallbackMethod = "ratingHotelFallback")
+//	@Retry(name = "ratingHotleBreaker", fallbackMethod = "ratingHotelFallback")
+	@RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
+	public ResponseEntity<Usermanagemant> getById(@PathVariable String id) {
 		try {
 			Usermanagemant byId = this.usermanagemantService.getById(id);
-			return new ResponseEntity<Usermanagemant>(byId, HttpStatus.OK);
+			return new ResponseEntity<>(byId, HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<Usermanagemant>( HttpStatus.INTERNAL_SERVER_ERROR);
+			// Log the exception for debugging
+			System.err.println("Error occurred: " + e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
 	}
-	
-	
+
+	public ResponseEntity<Usermanagemant> ratingHotelFallback(String id, Exception ex) {
+		// Log the fallback for debugging
+		System.out.println("Fallback method invoked due to: " + ex.getMessage());
+		Usermanagemant build = Usermanagemant.builder().name("rikesh").phone("8405099999").about("service is down")
+				.build();
+		return new ResponseEntity<>(build, HttpStatus.OK);
+	}
 
 }
